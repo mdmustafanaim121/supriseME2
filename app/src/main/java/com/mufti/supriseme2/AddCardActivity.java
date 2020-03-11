6e37e080c568948e71338641bd6d9cffb7d8f095
mdmustafanaim121 <mdmustafanaim121@gmail.com>
2020-03-11 15:02:58 -0500

    Completed all the optional task

--- app/src/main/java/com/mufti/supriseme2/AddCardActivity.java
+++ app/src/main/java/com/mufti/supriseme2/AddCardActivity.java
@@ -20,10 +20,14 @@ public class AddCardActivity extends AppCompatActivity {
 
         String s1 = getIntent().getStringExtra("key1");
         String s2 = getIntent().getStringExtra("key2");
+        String s3 = getIntent().getStringExtra("key3");
+        String s4 = getIntent().getStringExtra("key4");
+
 
         ((TextView)(findViewById(R.id.question))).setText(s1);
         ((TextView)(findViewById(R.id.answer))).setText(s2);
-
+        ((TextView)(findViewById(R.id.answer2))).setText(s3);
+        ((TextView)(findViewById(R.id.answer3))).setText(s4);
         (findViewById(R.id.cancelQ)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,16 +41,20 @@ public class AddCardActivity extends AppCompatActivity {
         (findViewById(R.id.saveQ)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String question,answer;
+                String question,answer1,answer2,answer3;
                 question = ((EditText)findViewById(R.id.question)).getText().toString();
-                answer = ((EditText)findViewById(R.id.answer)).getText().toString();
+                answer1 = ((EditText)findViewById(R.id.answer)).getText().toString();
+                answer2 = ((EditText)findViewById(R.id.answer2)).getText().toString();
+                answer3 = ((EditText)findViewById(R.id.answer3)).getText().toString();
 
-                if(question.equals("")|| answer.equals(""))
-                    Toast.makeText(AddCardActivity.this, "Question or Answer cannnot be empty", Toast.LENGTH_SHORT).show();
+                if(question.equals("")|| answer1.equals("")|| answer2.equals("")|| answer3.equals(""))
+                    Toast.makeText(AddCardActivity.this, "Question or Answers cannnot be empty", Toast.LENGTH_SHORT).show();
                 else{
                 Intent data = new Intent();
                 data.putExtra("qString",question);
-                data.putExtra("aString",answer);
+                data.putExtra("aString",answer1);
+                data.putExtra("aString1",answer2);
+                data.putExtra("aString2",answer3);
                 setResult(RESULT_OK,data);
                 finish();}
             }
--- app/src/main/java/com/mufti/supriseme2/MainActivity.java
+++ app/src/main/java/com/mufti/supriseme2/MainActivity.java
@@ -3,12 +3,14 @@ package com.mufti.supriseme2;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 
 import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -24,6 +26,7 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     String TAG="TAG";
+    private String mUser;
 
 
     //TextView question = (TextView)findViewById(R.id.suprise_question);
@@ -33,27 +36,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final String question,answer;
+        final String question,answer,answer2,answer3;
 
-
+        ((TextView) findViewById(R.id.suprise_question)).setText("What's 2+2 ?");
+        ((TextView) findViewById(R.id.suprise_answer1)).setText("4");
+        ((TextView) findViewById(R.id.suprise_answer2)).setText("5");
+        ((TextView) findViewById(R.id.suprise_answer3)).setText("8");
         //loop to check answers
-        findViewById(R.id.suprise_question).setOnClickListener(new View.OnClickListener() {
+       /* findViewById(R.id.suprise_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.suprise_question).setVisibility(View.INVISIBLE);
                 (findViewById(R.id.suprise_answer1)).setVisibility(View.VISIBLE);
             }
-        });
+        });*/
 
         findViewById(R.id.suprise_answer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.suprise_answer1).setVisibility(View.INVISIBLE);
-                (findViewById(R.id.suprise_question)).setVisibility(View.VISIBLE);
+                findViewById(R.id.suprise_answer1).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.correct));
+            }
+        });
+
+        findViewById(R.id.suprise_answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.suprise_answer1).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.correct));
+                findViewById(R.id.suprise_answer2).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.wrong));
+                findViewById(R.id.suprise_answer3).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.wrong));
+
+            }
+        });
+
+        findViewById(R.id.suprise_answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.suprise_answer1).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.correct));
+                findViewById(R.id.suprise_answer2).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.wrong));
+                findViewById(R.id.suprise_answer3).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.wrong));
+
             }
         });
 
 
+        findViewById(R.id.activityMain).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.suprise_answer1).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.reset));
+                findViewById(R.id.suprise_answer2).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.reset));
+                findViewById(R.id.suprise_answer3).setBackgroundColor(ContextCompat.getColor(MainActivity.this,R.color.reset));
+
+            }
+        });
 
 
 
@@ -67,12 +101,17 @@ public class MainActivity extends AppCompatActivity {
 
         question = ((TextView)(findViewById(R.id.suprise_question))).getText().toString();
         answer = ((TextView)(findViewById(R.id.suprise_answer1))).getText().toString();
+        answer2 = ((TextView)(findViewById(R.id.suprise_answer2))).getText().toString();
+        answer3 = ((TextView)(findViewById(R.id.suprise_answer3))).getText().toString();
+
         (findViewById(R.id.editQ)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent editClick = new Intent(MainActivity.this,AddCardActivity.class);
                 editClick.putExtra("key1",question);
                 editClick.putExtra("key2",answer);
+                editClick.putExtra("key3",answer2);
+                editClick.putExtra("key4",answer3);
                 MainActivity.this.startActivityForResult(editClick,121);
             }
         });
@@ -85,8 +124,12 @@ public class MainActivity extends AppCompatActivity {
         {
             String queUpdate = data.getExtras().getString("qString");
             String ansUpdate = data.getExtras().getString("aString");
+            String ansUpdate1 = data.getExtras().getString("aString1");
+            String ansUpdate2 = data.getExtras().getString("aString2");
             ((TextView)findViewById(R.id.suprise_question)).setText(queUpdate);
             ((TextView)findViewById(R.id.suprise_answer1)).setText(ansUpdate);
+            ((TextView)findViewById(R.id.suprise_answer1)).setText(ansUpdate1);
+            ((TextView)findViewById(R.id.suprise_answer1)).setText(ansUpdate2);
 
             Snackbar.make(findViewById(R.id.suprise_question),"Card updated!",
                     Snackbar.LENGTH_SHORT).show();
